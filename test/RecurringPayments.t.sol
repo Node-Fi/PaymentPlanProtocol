3439dc1c6166f0824cb8b3d5e1fa5521c115d6cd
Dylan Mooers <dylmooers@gmail.com>
2022-04-26 12:00:37 -0700

    add fuzzing

--- test/RecurringPayments.t.sol
+++ test/RecurringPayments.t.sol
@@ -15,6 +15,11 @@ contract ContractTest is DSTest {
         token = new ERC20PresetFixedSupply("Test", "TST", 10000000000000, address(this));
     }
 
+    function _createPlan(uint256 numIntervals, uint256 amountPerInterval, uint256 intervalLength) internal {
+        token.approve(address(payments), numIntervals * amountPerInterval);
+        payments.createPaymentPlan(address(this), address(token), amountPerInterval, numIntervals, intervalLength);
+    }
+
     function testFailCreateNoApproval() public {
         // Test trying to create a payment plan without approving the full amount
         payments.createPaymentPlan(address(this), address(token), 1000, 5, 1 days);
@@ -23,11 +28,18 @@ contract ContractTest is DSTest {
     function testCreatePlan() public {
         uint256 numIntervals = 5;
         uint256 amountPerInterval = 1000;
-        token.approve(address(payments), numIntervals * amountPerInterval);
-        payments.createPaymentPlan(address(this), address(token), amountPerInterval, numIntervals, 1 days);
+        uint256 intervalLength = 1 days;
+        _createPlan(numIntervals, amountPerInterval, intervalLength);
+
         PaymentStructures.PaymentSchedule memory plan = payments.getPaymentDetails(1);
         assert(plan.totalIntervals == numIntervals);
         assert(plan.amountPerInterval == amountPerInterval);
         assert(plan.nextTransferOn > block.timestamp);
+    }
+
+    function testFulfillPlanWithFuzzing(uint256 intervalLength) public {
+        uint256 numIntervals = 5;
+        uint256 amountPerInterval = 1000;
+        _createPlan(numIntervals, amountPerInterval, intervalLength);
     }
 }
